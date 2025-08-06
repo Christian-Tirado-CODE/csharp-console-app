fddd67545741cbb4793cebbd07decde17587d952
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-06 13:01:40 -0400

    Fix typo.

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -8,6 +8,6 @@ class Program
         int intMax = int.MaxValue;
         Console.WriteLine($"The range of integers is between {intMin} and {intMax}");
         Console.WriteLine($"If we add one to the max value it will cause an overflow condition so {intMax} plus one is {intMax + 1}.");
-        Console.WriteLine($"Subtracting one from the min value causes an underflow condition so {intMin} plus one is {intMin - 1}.");
+        Console.WriteLine($"Subtracting one from the min value causes an underflow condition so {intMin} minus one is {intMin - 1}.");
     }
 }
