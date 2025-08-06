bbbb07bbf85e0a5f33009bda561c0e2b1cb7aec8
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-06 13:29:36 -0400

    Working with the decimal type.

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -17,6 +17,26 @@ class Program
         Console.WriteLine($"The range of double value is between {doubleMin} and {doubleMax}.");
 
         // Be mindful of rounding errors. It can lead inaccurate outputs which have significant impact in financial and scientific applications.
-       // https://www.cs.drexel.edu/~popyack/Courses/CSP/Fa17/extras/Rounding/index.html
+        // https://www.cs.drexel.edu/~popyack/Courses/CSP/Fa17/extras/Rounding/index.html
+
+        decimal decimalMax = decimal.MaxValue;
+        decimal decimalMin = decimal.MinValue;
+
+        Console.WriteLine($"The range of the decimal type is from {decimalMin} to {decimalMax}.");
+
+        double a = 1.0;
+        double b = 3.0;
+        decimal c = 1.0M;
+        decimal d = 3.0M;
+        Console.WriteLine("Notice the difference in precision between the double and decimal type.");
+        Console.WriteLine("Double: " + a / b);
+        Console.WriteLine("Decimal: " + c / d);
+        /*
+        Additional resources:
+        - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
+        - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+        - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/numeric-conversions
+        
+        */
     }
 }
