4cf47535c9cfb80863f42879b51e0bd61612d8cc
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-06 12:43:10 -0400

    Working with integers

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -4,14 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        string myName = "Christian";
-        string myMothersName = "Mary";
-        string greeting = $"Hi! My name is {myName}! My Mother's name is {myMothersName} and it obviously consists of {myMothersName.Length} letters.         ";
-        string trimmedGreeting = greeting.Trim();
-        string askForWater = "If you want to ask for water in spanish then you can say the following: Can I please have some water?";
-        string askForWaterESTranslation = askForWater.Replace("Can I please have some water?", "Por favor me puedes traer agua?");
-        Console.WriteLine("Greeting: " + trimmedGreeting);
-        Console.WriteLine("Translation:" + askForWaterESTranslation);
-        Console.WriteLine("Does the askForWaterESTranslation variable have the word 'agua'?: " + askForWaterESTranslation.Contains("agua"));
+        int intMin = int.MinValue;
+        int intMax = int.MaxValue;
+        Console.WriteLine($"The range of integers is between {intMin} and {intMax}");
+        Console.WriteLine($"If we add one to the max value it will cause an overflow condition so {intMax} plus one is {intMax + 1}.");
+        Console.WriteLine($"Subtracting one from the min value causes an underflow condition so {intMin} plus one is {intMin - 1}.");
     }
 }
