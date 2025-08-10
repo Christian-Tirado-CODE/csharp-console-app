0309d26421acc659da4121d2ee73ec4904a1c852
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-10 15:17:18 -0400

    Explicit typing and inference.

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -4,14 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        string myName = "Christian";
-        string myMothersName = "Mary";
-        string greeting = $"Hi! My name is {myName}! My Mother's name is {myMothersName} and it obviously consists of {myMothersName.Length} letters.         ";
-        string trimmedGreeting = greeting.Trim();
-        string askForWater = "If you want to ask for water in spanish then you can say the following: Can I please have some water?";
-        string askForWaterESTranslation = askForWater.Replace("Can I please have some water?", "Por favor me puedes traer agua?");
-        Console.WriteLine("Greeting: " + trimmedGreeting);
-        Console.WriteLine("Translation:" + askForWaterESTranslation);
-        Console.WriteLine("Does the askForWaterESTranslation variable have the word 'agua'?: " + askForWaterESTranslation.Contains("agua"));
+
+        // Explicit types
+        float amount;
+        string message = "Hi!";
+        string[] names = ["Christian", "Brad", "Tyrone", "Lee", "Carl"];
+
+        // Type inference with the var keyword.
+        var namesBeginningWithC = from name in names where name.StartsWith('C') select name;
+        Console.WriteLine(string.Join(", ", namesBeginningWithC));
+
+
+        // Learn more about the IEnumerable interface: https://learn.microsoft.com/en-us/dotnet/api/system.collections.ienumerable?view=net-9.0
+
     }
 }
