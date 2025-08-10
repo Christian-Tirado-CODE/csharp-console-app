87a7b38c6a5b2762a0eac4bf5b4f210b98878e7f
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-10 14:12:27 -0400

    Working with the record type.

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,17 +1,31 @@
 ﻿namespace CSharpBasics;
+public record FinancialTransaction(
+        DateTime TransactionDate,
+        string Description,
+        decimal Amount,
+        string Currency,
+        string TransactionType
+    )
+    {
+        public void PrintTransaction() => Console.WriteLine($"Transaction Info: Date {TransactionDate}, Description {Description}, Amount {Amount} {Currency}, Type of Transaction {TransactionType}");
+    };
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        var employee1 = (id: 1, name: "Christian", department: "IT", age: 60);
-        Console.WriteLine($"Hi! My name is {employee1.name}, I'm {employee1.age} years old and I work at the {employee1.department} department.");
-        employee1.department = "Human Resources";
-        employee1.age = employee1.age + 1;
-        Console.WriteLine($"I want to inform you that I just turned {employee1.age}! Also, I moved to the {employee1.department} department.");
-        var employee2 = employee1 with { id = 2, name = "Karen" };
-        Console.WriteLine($"Hi! My name is {employee2.name}, I'm also {employee2.age} years old and work at the {employee1.department} department.");
+        FinancialTransaction ft = new FinancialTransaction(DateTime.Now, "low-key transfer ;)", 100.00M, "USD", "Transfer");
 
-        // Tuples have limited capabilities. That's why there are other types that define behavior. 
+        ft.PrintTransaction();
     }
+    //custom types: record, struct, class, interface and enum.
+    // Keep in mind the difference between a class and a struct.
+     /*
+        Additional resources:
+        - https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/
+        - https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/records
+        - https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/classes
+
+     */
 }
