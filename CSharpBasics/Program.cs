362c515a17fec7df91f5c99c8ee3ad376589963c
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-11 12:57:45 -0400

    Working with structs.

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,21 +1,35 @@
 ﻿namespace CSharpBasics;
 
+public readonly struct PictureFrame
+{
+    public PictureFrame(int width, int length, string shape, string color, string mountingType, string frameType)
+    {
+        Width = width;
+        Length = length;
+        Shape = shape;
+        Color = color;
+        MountingType = mountingType;
+        FrameType = frameType;
+
+
+    }
+
+    public int Width { get; init; }
+    public int Length { get; init; }
+    public string Shape { get; init; }
+    public string Color { get; init; }
+    public string MountingType { get; init; }
+    public string FrameType { get; init; }
+
+    public override string ToString() => $"Picture Frame: Dimensions={Length} X {Width}, Shape={Shape}, Color={Color}, MountingType={MountingType}, FrameType={FrameType}";
+
+    }  
 class Program
 {
     static void Main(string[] args)
     {
-
-        // Explicit types
-        float amount;
-        string message = "Hi!";
-        string[] names = ["Christian", "Brad", "Tyrone", "Lee", "Carl"];
-
-        // Type inference with the var keyword.
-        var namesBeginningWithC = from name in names where name.StartsWith('C') select name;
-        Console.WriteLine(string.Join(", ", namesBeginningWithC));
-
-
-        // Learn more about the IEnumerable interface: https://learn.microsoft.com/en-us/dotnet/api/system.collections.ienumerable?view=net-9.0
+        PictureFrame pf = new(8, 10, "Rectangular", "Black", "Wall mount", "Collage picture frame");
+        Console.WriteLine(pf.ToString());
 
     }
 }
